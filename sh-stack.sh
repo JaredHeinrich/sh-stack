a276a4999ab0cc52fbab62dcbc5d1e8ff28fbf5e
Jared Heinrich <jared.heinrich@sap.com>
2025-09-10 14:30:41 +0200

    Rm uninstall and sh-stack

--- sh-stack.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export CARGO_HOME="$REPO_DIR/cargo"
-
-$CARGO_HOME/bin/cargo run --release
--- uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-echo "Uninstalling sh-stack"
-REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-rm -rf $REPO_DIR
-echo "Uninstalled"
